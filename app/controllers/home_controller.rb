f78312485fb3c4251a202ccf1133a275533bfa30
Lewis Hollis <111375017+lewishollis@users.noreply.github.com>
2023-12-28 21:23:53 +0000

    work on homepage images started

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @image = Image.all
+    # Assuming your images are in the "app/assets/images/uploads/" directory
+    images_path = Rails.root.join("app", "assets", "images", "uploads")
+
+    # Get a list of image filenames
+    @image_filenames = Dir.glob(File.join(images_path, "*")).map { |path| File.basename(path) }
   end
 end
