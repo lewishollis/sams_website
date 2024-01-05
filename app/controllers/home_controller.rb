0c1614c43a54bfd176f2c85a98ba85c071886fe9
Lewis Hollis <111375017+lewishollis@users.noreply.github.com>
2024-01-05 16:57:31 +0100

    image uploads working and css gallery beginning

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    # Assuming your images are in the "app/assets/images/uploads/" directory
-    images_path = Rails.root.join("app", "assets", "images", "uploads")
-
-    # Get a list of image filenames
-    @image_filenames = Dir.glob(File.join(images_path, "*")).map { |path| File.basename(path) }
-  end
-end
