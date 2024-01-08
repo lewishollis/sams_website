c37ab9b5807d01a7963272e5f5f43c1008d693c6
Lewis Hollis <111375017+lewishollis@users.noreply.github.com>
2024-01-08 17:41:06 +0000

    routes reverse

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get '/home/index'
-  root to: '/pages#home'
-  get '/work', to: '/pages#work'
-  get '/about', to: '/pages#about'
-  get '/showreel', to: '/pages#showreel'
+  root to: "pages#home"
+  get 'work', to: 'pages#work'
+  get 'about', to: 'pages#about'
+  get 'showreel', to: 'pages#showreel'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
