1a1bcc49ced38173e5088470ada9435d5953e314
Lewis Hollis <111375017+lewishollis@users.noreply.github.com>
2024-01-08 17:38:37 +0000

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root to: "pages#home"
-  get 'work', to: 'pages#work'
-  get 'about', to: 'pages#about'
-  get 'showreel', to: 'pages#showreel'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/home/index'
+  root to: '/pages#home'
+  get '/work', to: '/pages#work'
+  get '/about', to: '/pages#about'
+  get '/showreel', to: '/pages#showreel'
 end
